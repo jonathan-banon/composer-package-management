3f4843e3fcd0502e76cb9e24109fb806d1169b82
banon jonathan <j.banon01@gmail.com>
2021-04-15 00:24:23 +0200

    first commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '481f2f7b159fe65ce16efbdcadd605ab22f89697',
+    'name' => 'banon/quete_manager',
+  ),
+  'versions' => 
+  array (
+    'banon/quete_manager' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '481f2f7b159fe65ce16efbdcadd605ab22f89697',
+    ),
+    'ehime/hello-world' => 
+    array (
+      'pretty_version' => '1.0.5',
+      'version' => '1.0.5.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b1c8cdd2c11272d8c5deec7816e51fa5374217c1',
+    ),
+  ),
+);
